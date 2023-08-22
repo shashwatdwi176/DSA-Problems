f7f1c9cb82f9f6abb345d22041c189078b88ffb2
shashwatdwi176 <dwivedishashwat176@gmail.com>
2023-08-22 10:52:51 +0530

    brute force approach

---
+++ Set-matrix-zeres.cpp
@@ -0,0 +1,35 @@
+//Brute force approach
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+vector<vector<int>> zeroMatrix (vector<vector<int>> &matrix, int n, int m)
+{
+    for (int i=0; i<n;i++){
+        for (int j=0;j<m;j++){
+            if (matrix[i][j]==0){
+               for (int k=0;k<n;k++){
+                if (matrix[i][k]!=0){
+                    matrix[i][k]=-1;
+                }
+               }
+               for (int h=0;h<m;h++){
+                if (matrix[h][j]!=0){
+                    matrix[h][j]=-1;
+                }
+               }
+            }
+        }
+    }
+
+    for (int i=0 ; i<n;i++){
+        for (int j=0;j<m;j++){
+            if (matrix[i][j]==-1){
+                matrix[i][j]=0;
+            }
+        }
+    }
+}
+
