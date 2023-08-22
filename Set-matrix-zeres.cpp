708ad111f2b765ab83db37c7b75842e6928c6dcf
shashwatdwi176 <dwivedishashwat176@gmail.com>
2023-08-23 00:25:36 +0530

    added better approach

--- Set-matrix-zeres.cpp
+++ Set-matrix-zeres.cpp
@@ -33,3 +33,26 @@ vector<vector<int>> zeroMatrix (vector<vector<int>> &matrix, int n, int m)
     }
 }
 
+vector<vector<int>> zeroMatrix (vector<vector<int>> &matrix, int n, int m)
+{
+   int col[m]={0};
+   int row[n]={0};
+
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            if (matrix[i][j]==0){
+                row[i]=1;
+                col[j]=1;
+            }
+        }
+    }
+
+    for (int i=0;i<n;i++){
+        for (int j=0; j<m;j++){
+            if (row[i]==1 || col[j]==1){
+                matrix[i][j]=0;
+            }
+        }
+    }
+    return matrix;
+}
